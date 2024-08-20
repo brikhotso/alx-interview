4723afdadda88c9a9a56aafc7c9c69e1c462987e
brikhotso <brenda.rikhotso@gmail.com>
2024-08-20 18:24:43 +0200

    fewest number of coins needed to meet a given amount total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""0-making_change"""
+
+
+def makeChange(coins, total):
+    """ Make change on Coins"""
+    # Sort coins in descending order
+    coins.sort(reverse=True)
+
+    # Initialize number of coins
+    num_coins = 0
+
+    # Try to use each coin
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
+
+    if total == 0:
+        return num_coins
+    else:
+        return -1
